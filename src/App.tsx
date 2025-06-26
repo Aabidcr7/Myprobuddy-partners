347d83a614c4ee0229a191b671757106acc709ee
Aabidcr7 <aabidibrahim2004@gmail.com>
2025-06-26 12:33:43 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
+import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
 import AffliatePartners from "./pages/affiliate-partners";
 import ServicePartnerForm from "./pages/service-partners";
 import NotFound from "./pages/NotFound";
+
+// Declare global gtag type
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+  }
+}
+
+// GA tracking wrapper
+const AppRoutesWithGA = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (typeof window.gtag === "function") {
+      window.gtag("config", "G-GEXN3H3TEC", {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/affiliate-partners" element={<AffliatePartners />} />
+      <Route path="/service-partners" element={<ServicePartnerForm />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
 
 const queryClient = new QueryClient();
 
@@ -16,13 +46,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/affiliate-partners" element={<AffliatePartners />} />
-          <Route path="/service-partners" element={<ServicePartnerForm />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AppRoutesWithGA />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
